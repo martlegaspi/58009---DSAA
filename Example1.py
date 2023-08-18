0a65ed7b5921c677636e67e0fd8186221616ea7c
martlegaspi <98940218+martlegaspi@users.noreply.github.com>
2023-08-18 12:26:55 +0800

    Add files via upload

---
+++ Example1.py
@@ -0,0 +1,6 @@
+L = float(input("Enter the length of the rectangle: "))
+W = float(input("Enter the width of the rectangle: "))
+
+area = (L*W)
+
+print("The area of the rectangle is: " + str("%.2f" % area))
